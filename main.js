8bb2fbe02267914705a8c36b110787e19ee6fbc0
Moon Rider <itachi0777@mail.ru>
2023-10-06 09:35:56 +0500

    JavaScript

---
+++ main.js
@@ -0,0 +1,311 @@
+// Integer 1 //
+
+// const L = parseInt(prompt("Indicate the height in centimetres:"));
+
+// const m = (L / 100);
+
+// const fullMeters = Math.floor(m);
+
+// alert(`Full meters: ${fullMeters}`); 
+
+
+
+// Integer 2 //
+
+// const M = parseInt(prompt("Indicate the weight in kg:"));
+
+// const t = (M / 1000);
+
+// const fullTons = Math.floor(t);
+
+// alert(`Full tons: ${fullTons}`); 
+
+
+
+// Integer 3 //
+
+// const B = parseInt(prompt("Indicate the size in bite:"));
+
+// const kb = (B / 1024);
+
+// const fullKbs = Math.floor(kb);
+
+// alert(`Full kbs: ${fullKbs}`); 
+
+
+
+// Integer 4 //
+
+// const A = parseInt(prompt('Enter a number equal to A'));
+
+// const B = parseInt(prompt('Enter a number equal to B'));
+
+// if (A > B) {
+//     const x = A / B;
+//     const y = Math.floor(x);
+//     alert(`A is greater than B. Result: ${y}`);
+//   } else {
+//     alert('A is not greater than B.');
+//   }
+
+
+
+// Integer 5 //
+
+// const A = parseInt(prompt('Enter a number equal to A'));
+
+// const B = parseInt(prompt('Enter a number equal to B'));
+
+// if (A > B) {
+//     const x = A / B;
+//     alert(`A is greater than B. Result: ${x}`);
+//   } else {
+//     alert('A is not greater than B.');
+//   }
+
+
+
+// Integer 6 //
+
+// const A = parseInt(prompt('Enter two-digit number'));
+
+// if (A >= 10 && A < 100) {
+//     const B = Math.floor(A / 10);
+//     const C = A % 10;
+
+//     alert(`Ten number: ${B}, Unit number: ${C}`);
+// } else {
+//     alert(`Please enter only two-digit number`);
+// }
+
+
+
+// Integer 7 //
+
+// const A = parseInt(prompt('Enter two-digit number'));
+
+// if (A >= 10 && A < 100) {
+//     const B = Math.floor(A / 10);
+//     const C = A % 10;
+//     const D = B + C;
+    
+//     alert(`Folded number: ${D}`);
+// } else {
+//     alert(`Please enter only two-digit number`);
+// }
+
+
+
+// Integer 8 //
+
+// const A = parseInt(prompt('Enter two-digit number'));
+
+// if (A >= 10 && A < 100) {
+//     const B = Math.floor(A / 10);
+//     const C = A % 10;
+
+//     alert(`${C}${B}`);
+// } else {
+//     alert(`Please enter only two-digit number`);
+// }
+
+
+
+// Integer 19 //
+
+// const N = parseInt(prompt("Enter the number: "));
+
+// const M = Math.floor(N / 60);
+
+// alert(`Full minutes: ${M}`); 
+
+
+
+// Integer 20 // 
+
+// const N = parseInt(prompt("Enter the number: "));
+
+// const H = Math.floor(N / 3600);
+
+// alert(`Full hours: ${H}`); 
+
+
+
+// Integer 21 // 
+
+// const N = parseInt(prompt("Enter the number: "));
+
+// const M = Math.floor(N / 60);
+
+// const S = N % 60;
+
+// alert(`${M} minutes, and ${S} seconds`); 
+
+
+
+// Integer 22 // 
+
+// const N = parseInt(prompt("Enter the number: "));
+
+// const H = Math.floor(N / 3600);
+
+// const S = N % 3600;
+
+// alert(`${H} hours, and ${S} seconds`); 
+
+
+
+// Integer 23 //
+
+// const N = parseInt(prompt("Enter the number: "));
+
+// const H = Math.floor(N / 3600);
+
+// const M = Math.floor((N / 60) / 60);
+
+// const S = N % 60;
+
+// alert(`${H} hours, ${M} minutes, and ${S} seconds`);
+
+
+
+// Begin 1 //
+
+// const A = parseInt(prompt("Indicate the side of the square: "));
+
+// const P = A * 4;
+
+// alert(`The perimeter: ${P}`);
+
+
+
+// Begin 2 // 
+
+// const A = parseInt(prompt("Indicate the side of the square: "));
+
+// const S = A * A;
+
+// alert(`The surface: ${S}`);
+
+
+
+// Begin 3 //
+
+// const A = parseInt(prompt("Indicate the side of the square: "));
+// const B = parseInt(prompt("Indicate the side of the square: "));
+
+// const S = A * B;
+
+// const P = 2 * (A + B);
+
+// alert(`The surface: ${S}, The perimeter: ${P}`);
+
+
+
+// Begin 4 /// 
+
+// const D = parseInt(prompt("Indicate the diameter of the circle: "));
+
+// const P = 3.14;
+
+// const L = P * D;
+
+// alert(`Diameter height of the circle: ${L}`)
+
+
+
+// Begin 5 //
+
+// const A = parseInt(prompt("Indicate the side of the cube: "));
+
+// const V = A * A * A;
+
+// const S = 6 * A * A;
+
+// alert(`Sphere of the cube: ${S}`)
+
+
+
+// Begin 6 // 
+
+// const A = parseInt(prompt("Indicate the sides of the parallelepiped: "));
+// const B = parseInt(prompt("Indicate the sides of the parallelepiped: "));
+// const C = parseInt(prompt("Indicate the sides of the parallelepiped: "));
+
+// const V = A * B * C;
+
+// const S = 2 * (A * B + B * C + A * C);
+
+// alert(`Sphere of the parallelepiped: ${S}`)
+
+
+
+// Begin 7 //
+
+// const R = parseInt(prompt("Indicate the radius of the circle: "));
+
+// const P = 3.14;
+
+// const L = 2 * P * R;
+
+// const S = P * R * R;
+
+// alert(`The height of the circle: ${L}, The side of the circle ${S}`);
+
+
+
+// Begin 8 // 
+
+// const A = parseInt(prompt("Enter the numbers: "));
+// const B = parseInt(prompt("Enter the numbers: "));
+
+// const S = (A + B) / 2;
+
+// alert(`The arithmetic: ${S}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
